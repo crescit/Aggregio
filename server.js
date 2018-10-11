5e2140d70f3bc216a94354a910746d135b6aed23
Josh Jaquez <jjaquez@wisc.edu>
2018-10-11 10:07:49 -0700

    add back end, add user schema, add validation for login and register routes

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+
+const app = express();
+
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to mongodb
+mongoose
+    .connect(db)
+    .then( () => console.log('MongoDb Connected'))
+    .catch( err => console.log(err))
+
+//Passport Middleware
+app.use(passport.initialize());
+
+//Passport Config
+require('./config/passport')(passport);
+
+//use routes, this is how the the files in api know that they're pointing to /api/x
+app.use('/api/users', users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
